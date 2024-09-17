ab3b964d8c65f5a99f29978f646716ca4f5a8866
ifedayo <ifedayyo12@gmail.com>
2024-09-17 13:33:34 +0100

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import HomePage from "./HomePage";
+import
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <HomePage />
+
+     
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <NavBar />
+      <FootBar />
+
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/aboutme" element={<AboutMe />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="*" element={<PageNotFound />} />
+        {/* <Route path="firstproject" element={<FirstProject />} />
+         */}
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <img
+        className="logo"
+        src={logo}
+        alt="Portfolio"
+        width="100"
+        height="50"
+      />
+
+      <Link to="/" activeClassName="active">
+        Home
+      </Link>
+      <Link to="/aboutme" activeClassName="active">
+        About{" "}
+      </Link>
+      <Link to="/portfolio" activeClassName="active">
+        Portfolio
+      </Link>
+    </div>
+  );
+}
+
+function FootBar() {
+  return <div></div>;
+}
+
+      {/** <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +82,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> **/}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
