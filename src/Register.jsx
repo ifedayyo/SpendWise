b17ea80ef9cbe397d6782872d78ffd05242a1e99
ifedayo <ifedayyo12@gmail.com>
2024-09-22 17:45:24 +0100

    push

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,8 +1,13 @@
 function Register() {
   return (
-    <div>
-      <p> Register with us and manage your money welllaaaaaaaaaaaa</p>
-    </div>
+    <form>
+      <label htmlFor="fname">First name: </label> <br />
+      <input type="text" id="fname" name="fname" required />
+      <label htmlFor="lname">Last nme: </label> <br />
+      <input type="text" id="lname" name="lname" required />
+      <label htmlFor="email">Enter your email: </label> <br />
+      <input type="email" id="email" name="email" required />
+    </form>
   );
 }
 
