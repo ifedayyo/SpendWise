05d3fd3983dce3b32836eb3ff59301373e20c5ec
ifedayo <ifedayyo12@gmail.com>
2024-09-22 15:04:13 +0100

    push

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -11,11 +11,11 @@ export default function HomePage() {
           <p className="descriptionparagraph">
             {" "}
             The month is not over and you are already owing PalmPay. We know you
-            want better for yourself. You want to make the most of every single
-            kobo. You want to track how money trinkles in and out of your
-            account. You want to save for the rainy day too. You don't want to
-            eat your future. <br /> We know. <br /> We know you want to{" "}
-            <strong> SpendWise</strong>.
+            want better for yourself. <br />
+            You want to make the most of every single kobo. You want to track
+            how money trinkles in and out of your account. You want to save for
+            the rainy day too. You don't want to eat your future. <br /> We
+            know. <br /> We know you want to <strong> SpendWise</strong>.
           </p>
 
           <button className="descriptionbutton">Get Started</button>
@@ -27,8 +27,8 @@ export default function HomePage() {
               <a href="FirstProject.js">
                 <img
                   className="imageone"
-                  src="https://assets.website-files.com/5c6e6f46bf98b2013cf621c5/5cdbececb102f913e509144f_portfolio%201%20-%20wide.svg"
-                  alt="Project_1"
+                  src="/assets/a-laptop-showing-charts-on-the-screen.jpg"
+                  alt="a laptop showing charts on the screen"
                 />
               </a>
             </div>
@@ -47,8 +47,8 @@ export default function HomePage() {
               <a href="FirstProject.js" className="linktoprojectone">
                 <img
                   className="imagetwo"
-                  src="https://assets.website-files.com/5c6e6f46bf98b2013cf621c5/5cdbececb102f913e509144f_portfolio%201%20-%20wide.svg"
-                  alt="Project_1"
+                  src="assets/boxes-with-the-word-budget-on-them.jfif"
+                  alt="boxes with the word budget on them"
                 />
               </a>
             </div>
@@ -63,44 +63,8 @@ export default function HomePage() {
         </div>
         <div className="projectcontainer">
           {/*third project*/}
-          <div className="firstchildcontainer">
-            <div>
-              <a href="FirstProject.js" className="linktoprojectone">
-                <img
-                  className="imagetwo"
-                  src="https://assets.website-files.com/5c6e6f46bf98b2013cf621c5/5cdbececb102f913e509144f_portfolio%201%20-%20wide.svg"
-                  alt="Project_1"
-                />
-              </a>
-            </div>
-
-            <div className="projectone">
-              {" "}
-              <a href="FirstProject.js">Project 1</a>{" "}
-            </div>
-
-            <div className="flutterproject">Flutter Project</div>
-          </div>
 
           {/*fourth project*/}
-          <div className="firstchildcontainer">
-            <div>
-              <a href="FirstProject.js" className="linktoprojectone">
-                <img
-                  className="imagetwo"
-                  src="https://assets.website-files.com/5c6e6f46bf98b2013cf621c5/5cdbececb102f913e509144f_portfolio%201%20-%20wide.svg"
-                  alt="Project_1"
-                />
-              </a>
-            </div>
-
-            <div className="projectone">
-              {" "}
-              <a href="FirstProject.js">Project 1</a>{" "}
-            </div>
-
-            <div className="flutterproject">Flutter Project</div>
-          </div>
         </div>
       </div>
     </div>
