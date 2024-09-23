206db9230aeed2803a69826296a014837683aaef
ifedayo <ifedayyo12@gmail.com>
2024-09-23 13:55:08 +0100

    push

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,13 +1,20 @@
 function Register() {
   return (
-    <form>
-      <label htmlFor="fname">First name: </label> <br />
-      <input type="text" id="fname" name="fname" required />
-      <label htmlFor="lname">Last nme: </label> <br />
-      <input type="text" id="lname" name="lname" required />
-      <label htmlFor="email">Enter your email: </label> <br />
-      <input type="email" id="email" name="email" required />
-    </form>
+    <>
+      <p>
+        Hey there - this is to let you know that we are proud of you for taking
+        the first step to <strong> SpendWise</strong>. So, chin up! The best is
+        about to come.{" "}
+      </p>
+      <form>
+        <label htmlFor="fname">First name: </label> <br />
+        <input type="text" id="fname" name="fname" required />
+        <label htmlFor="lname">Last nme: </label> <br />
+        <input type="text" id="lname" name="lname" required />
+        <label htmlFor="email">Enter your email: </label> <br />
+        <input type="email" id="email" name="email" required />
+      </form>
+    </>
   );
 }
 
