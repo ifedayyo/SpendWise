8b0c45bb72952100396c7ec3184668dcdeb46ad1
ifedayo <ifedayyo12@gmail.com>
2024-09-24 14:30:52 +0100

    push

--- src/Register.jsx
+++ src/Register.jsx
@@ -9,39 +9,33 @@ function Register() {
         about to come.{" "}
       </p>
       <form className="form-styling">
-        <label htmlFor="fname" className="label-styling">
-          First name:{" "}
-        </label>{" "}
-        <br />
         <input
           type="text"
           id="fname"
           className="input-styling"
           name="fname"
+          placeholder="First name:"
           required
         />
-        <label htmlFor="lname" className="label-styling">
-          Last name:{" "}
-        </label>{" "}
-        <br />
+
         <input
           type="text"
           id="lname"
           className="input-styling"
           name="lname"
+          placeholder="Last name"
           required
         />
-        <label htmlFor="email" className="label-styling">
-          Enter your email:{" "}
-        </label>{" "}
-        <br />
+
         <input
           type="email"
           id="email"
           className="input-styling"
           name="email"
+          placeholder="Enter your email"
           required
         />
+        <button className="button-styling">Submit</button>
       </form>
     </>
   );
