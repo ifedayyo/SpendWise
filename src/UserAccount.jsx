ef2a9204e17778e5fee7bc316da3cff5587087fe
ifedayo <ifedayyo12@gmail.com>
2024-09-24 15:47:53 +0100

    push

--- src/UserAccount.jsx
+++ src/UserAccount.jsx
@@ -1,7 +1,9 @@
 function UserAccount() {
   return (
     <div>
-      <p> Here is your account</p>
+      <div>
+        <p> 2024-08-09</p>
+      </div>
     </div>
   );
 }
@@ -12,11 +14,17 @@ export default UserAccount;
   okay, here is the sort of budget app i want to create for
   myself:
   1. have a list of items i spend money on e.g. shoes, bags,
-  food, transportation, black tax, emergency
+  food, transportation, black tax, emergency.
+
+  here, there will be a drop-down button from which users
+  can choose. and there choice will be immediately added
+  to an empty array/list
+
   2. amount i earn (it should be connected to my bank account)
-  3. set a cap limit for each item which must no be exceeded
+  3. set a cap limit for each item which must not be exceeded
   4. show a display of amount that will be left after removing
   the amount i will spend on list of items
-  5. 
+  5. get a warning if i am 10% close to the set limit
+  6. 
   */
 }
