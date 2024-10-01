f8e14b219ffd673bd8698d2ed2cf5f2288c60938
ifedayo <ifedayyo12@gmail.com>
2024-10-01 11:04:02 +0100

    push

--- src/Calculate.jsx
+++ src/Calculate.jsx
@@ -1,4 +1,13 @@
+import { Textfit } from "react-textfit";
 import ".index/css";
 export default function Calculate() {
-  return <div className="calculator-container"></div>;
+  return <div className="calculator-container"> children </div>;
+}
+
+function Screen() {
+  return (
+    <Textfit className="screen" mode="single" max={70}>
+      value
+    </Textfit>
+  );
 }
