a4cfaa2911ce62520ddd481e27c4cff6fcb722aa
ifedayo <ifedayyo12@gmail.com>
2024-10-03 15:13:41 +0100

    push

--- src/Calculate.jsx
+++ src/Calculate.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import "./index.css";
 
 const btnValues = [
-  ["C", "+-", "%", "/"],
+  ["C", "%", "/"],
   [7, 8, 9, "X"],
   [4, 5, 6, "-"],
   [1, 2, 3, "+"],
@@ -13,24 +13,75 @@ const btnValues = [
 ];
 export default function Calculate() {
   const [screenValue, setScreenValue] = useState("0");
+  const [previousValue, setPreviousValue] = useState(null);
+  const [operator, setOperator] = useState(null);
 
   //the function below will handle button clicks
   const handleButtonClick = (btn) => {
-    console.log(`${btn} clicked`);
+    //If it is a number
+    if (!isNaN(btn)) {
+      setScreenValue((previousValue) =>
+        previousValue === "0" ? String(btn) : previousValue + String(btn)
+      );
+    }
 
-    setScreenValue((prevValue) => prevValue + btn);
+    //If it is an operator
+    if (["+", "-", "x", "/"].includes(btn)) {
+      setOperator(btn);
+      setPreviousValue(screenValue);
+      setScreenValue("0");
+    }
+
+    //If it is the equal (=) sign, perform the calculation
+    if (btn === "=") {
+      if (operator && previousValue) {
+        calculateResult();
+      }
+    }
+
+    if (btn === "C") {
+      resetCalculator();
+    }
+  };
+
+  const calculateResult = () => {
+    const prev = parseFloat(previousValue);
+    const current = parseFloat(screenValue);
+    let result;
+
+    switch (operator) {
+      case "+":
+        result = prev + current;
+        break;
+      case "-":
+        result = prev - current;
+        break;
+      case "X":
+        result = prev * current;
+        break;
+      case "/":
+        result = prev / current;
+        break;
+      default:
+        return;
+    }
+    setScreenValue(String(result));
+    setOperator(null);
+    setPreviousValue(null);
   };
   return (
     <div className="calculator-container">
       <Screen value={screenValue} />
       <ButtonBox>
         {btnValues.flat().map((btn, i) => {
-          <Button
-            key={i}
-            className={btn === "=" ? "equals" : ""}
-            value={btn}
-            onClick={() => handleButtonClick(btn)}
-          />;
+          return (
+            <Button
+              key={i}
+              className={btn === "=" ? "equals" : ""}
+              value={btn}
+              onClick={() => handleButtonClick(btn)}
+            />
+          );
         })}
       </ButtonBox>
     </div>
@@ -55,9 +106,9 @@ ButtonBox.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-function Button({ value, onClick, className }) {
+function Button({ value, onClick }) {
   return (
-    <button className={`'button ${className}`} onClick={onClick}>
+    <button className="button-calculator" onClick={onClick}>
       {value}
     </button>
   );
